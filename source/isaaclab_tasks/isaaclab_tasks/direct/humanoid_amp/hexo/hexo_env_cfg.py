44d83e5b1995eb74fdddc49ac726fd0d603e62b7
Bowen <Bowen@lbw.com>
2025-08-20 14:46:24 +0800

    feat(hexo-env): enable full action control and expand exo observations
    - Re-enabled action application for humanoid and exo agents, allowing them to respond to control inputs.
    - Expanded the exo observation space to include linear acceleration and angular velocity from both left and right knee IMUs.
    - Increased the number of parallel simulation environments from 1 to 4096 for improved training scalability.
    - Refactored IMU sensor configuration to explicitly define separate left and right leg IMUs.
    - Updated the exo observation space size in the environment configuration to reflect the new sensor data.

--- source/isaaclab_tasks/isaaclab_tasks/direct/humanoid_amp/hexo/hexo_env_cfg.py
+++ source/isaaclab_tasks/isaaclab_tasks/direct/humanoid_amp/hexo/hexo_env_cfg.py
@@ -30,7 +30,7 @@ class HexoEnvCfg(DirectMARLEnvCfg):
 
     possible_agents = ["exo", "humanoid" ]
     action_spaces = {"exo": 2, "humanoid": 12}
-    observation_spaces = {"exo": 4, "humanoid": 49+6}
+    observation_spaces = {"exo": 14, "humanoid": 49+6}
     state_space = -1
     num_amp_observations = 2
     # 7 + 3 + 3 + 12 + 12+ 12+  12+6
@@ -55,7 +55,7 @@ class HexoEnvCfg(DirectMARLEnvCfg):
 
 
     # scene
-    scene: InteractiveSceneCfg = InteractiveSceneCfg(num_envs=1, env_spacing=8.0, replicate_physics=True)
+    scene: InteractiveSceneCfg = InteractiveSceneCfg(num_envs=4096, env_spacing=8.0, replicate_physics=True)
 
     # robot
     robot_cfg: ArticulationCfg = HEXO_CFG.replace(prim_path="/World/envs/env_.*/Robot")
@@ -76,16 +76,27 @@ class HexoEnvCfg(DirectMARLEnvCfg):
                           "right_ankle_roll_joint"
                         ]
 
-    leg_imu_cfg = ImuCfg(
+    right_leg_imu_cfg = ImuCfg(
 
-        prim_path="/World/envs/env_.*/Robot/*_knee_link",  
+        prim_path="/World/envs/env_.*/Robot/right_knee_link",  
         update_period=0.01,   # 100 Hz
         history_length=16,
         # visualizer_cfg= RED_ARROW_X_MARKER_CFG.replace(prim_path="Visuals/Command/velocity_goal"),
         # visualizer_cfg= RED_ARROW_X_MARKER_CFG.replace(prim_path="/World/envs/env_*/Robot/left_knee_link/IMU_visual")
     )
-    leg_imu_cfg.offset.pos = (0.0, 0.0, -0.3)  # 调整 IMU 在膝盖上的位置
+    right_leg_imu_cfg.offset.pos = (0.0, 0.0, -0.3)  # 调整 IMU 在膝盖上的位置
     # leg_imu_cfg.debug_vis = True
+
+
+    left_leg_imu_cfg = ImuCfg(
+
+        prim_path="/World/envs/env_.*/Robot/left_knee_link",  
+        update_period=0.01,   # 100 Hz
+        history_length=16,
+        # visualizer_cfg= RED_ARROW_X_MARKER_CFG.replace(prim_path="Visuals/Command/velocity_goal"),
+        # visualizer_cfg= RED_ARROW_X_M
+    )
+    left_leg_imu_cfg.offset.pos = (0.0, 0.0, -0.3)
 
 
     # # action scales
